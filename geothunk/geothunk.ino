d3be0044753dfa25312969da1fadcf2127ee0988
Brad Midgley <bmidgley@gmail.com>
2017-04-28 16:30:05 -0600

    rm comments

--- geothunk/geothunk.ino
+++ geothunk/geothunk.ino
@@ -5,9 +5,7 @@
 #include <DNSServer.h>
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
-// select wich pin will trigger the configuraton portal when set to LOW
-// ESP-01 users please note: the only pins available (0 and 2), are shared 
-// with the bootloader, so always set them HIGH at power-up
+bool shouldSaveConfig = false;
 #define TRIGGER_PIN 0
 
 void configure(WiFiManager &wifiManager) {
@@ -21,7 +19,6 @@ void configure(WiFiManager &wifiManager) {
 }
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println("\n Starting");
 
@@ -30,33 +27,9 @@ void setup() {
 
 
 void loop() {
-  // is configuration portal requested?
   if ( digitalRead(TRIGGER_PIN) == LOW ) {
-    //WiFiManager
-    //Local intialization. Once its business is done, there is no need to keep it around
     WiFiManager wifiManager;
-
-    //reset settings - for testing
-    //wifiManager.resetSettings();
-
-    //sets timeout until configuration portal gets turned off
-    //useful to make it all retry or go to sleep
-    //in seconds
-    //wifiManager.setTimeout(120);
-
-    //it starts an access point with the specified name
-    //here  "AutoConnectAP"
-    //and goes into a blocking loop awaiting configuration
-
-    //WITHOUT THIS THE AP DOES NOT SEEM TO WORK PROPERLY WITH SDK 1.5 , update to at least 1.5.1
-    //WiFi.mode(WIFI_STA);
     configure(wifiManager);
-
-    //if you get here you have connected to the WiFi
     Serial.println("connected...yeey :)");
   }
-
-
-  // put your main code here, to run repeatedly:
-
 }
