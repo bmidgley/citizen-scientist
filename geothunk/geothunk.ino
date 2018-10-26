94c0389690e54b365442334c47c6287729cde6c5
Brad Midgley <bmidgley@gmail.com>
2018-10-26 10:28:10 -0600

    Bump version and make on-device graph go no smaller than 15ug/m3 scale

--- geothunk/geothunk.ino
+++ geothunk/geothunk.ino
@@ -43,7 +43,7 @@ SimpleDHT11 dht11;
 
 #define MDNS_NAME "geothunk"
 #define TRIGGER_PIN 0
-#define VERSION "1.20"
+#define VERSION "1.21"
 #define POINTS 128
 
 bool shouldSaveConfig = false;
@@ -249,7 +249,7 @@ char *how_good(unsigned int v) {
 }
 
 void graph_set(unsigned short int *a, int points, int p0, int p1, int idx) {
-  int max = 0;
+  int max = 15;
 
   for (int i = 0; i < points; i++) {
     if (max < a[i]) max = a[i];
