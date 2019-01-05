e6b5fe98eb5b4508a980a5cbaf45c3e185936f8d
Brad Midgley <bmidgley@gmail.com>
2019-01-04 19:47:16 -0700

    Revert "Newer lcd library flips screen for us"
    This reverts commit cb45a926f348bbc1108baa7a86e9c05a396e7885.

--- geothunk/geothunk.ino
+++ geothunk/geothunk.ino
@@ -45,7 +45,7 @@ SimpleDHT11 dht11;
 #define TRIGGER_PIN 0
 #define LED_PIN D4
 #define PULSE_PIN D8
-#define VERSION "1.22"
+#define VERSION "1.21"
 #define POINTS 128
 
 bool shouldSaveConfig = false;
@@ -427,6 +427,9 @@ void setup() {
   myservo.attach(D0);
 
   display.init();
+#ifndef SPI_DISPLAY
+  display.flipScreenVertically();
+#endif
   display.setContrast(255);
   display.clear();
 
